a0f120b45b32cb2aeb869ab67d922cd88409803b
daisy <1656505353@qq.com>
2015-01-25 21:24:48 +0800

    change log
    1 改进查询机制为增量查询，增量时间段为1个小时。暂时没有发现BUG

--- test/TBATHandler.py
+++ test/TBATHandler.py
@@ -7,12 +7,20 @@ g_url = "gw.api.tbsandbox.com"
 def send_goods(client_id="1023079608",client_secret="sandbox0522e2394ad8813381ce7f457",access_token="6101f21f5df655ca131999e3b72ef19b085defe92c85d213651880146"):
     while True:
         print("====>>NEW LOOP")
+        # import pdb;pdb.set_trace()
         #=====================================get all of trades===============================#
-        req_get_all_trade = top.api.TradesSoldGetRequest(g_url)
-        # req_get_all_trade=top.api.TradesSoldIncrementGetRequest(g_url)
+        # req_get_all_trade = top.api.TradesSoldGetRequest(g_url)#get all trades
+        req_get_all_trade=top.api.TradesSoldIncrementGetRequest(g_url)#get increament trades
         req_get_all_trade.set_app_info(top.appinfo(client_id, client_secret))
         req_get_all_trade.fields = "seller_nick,buyer_nick,title,type,created,sid,tid,seller_rate,buyer_rate,\
                             status,payment,discount_fee,adjust_fee,post_fee,total_fee,pay_time"
+        # set start and end time
+        t_cur_time = time.time()
+        t_start_time = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(t_cur_time-60*60))
+        t_end_time = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(t_cur_time))
+        req_get_all_trade.start_modified = t_start_time
+        req_get_all_trade.end_modified = t_end_time
+        # req_get_all_trade.status = "WAIT_SELLER_SEND_GOODS"#only process WAIT_SELLER_SEND_GOODS trades
         #=====================================get a trade full info============================#
         req_get_one_trade = top.api.TradeFullinfoGetRequest(g_url)
         req_get_one_trade.set_app_info(top.appinfo(client_id, client_secret))
@@ -23,19 +31,20 @@ def send_goods(client_id="1023079608",client_secret="sandbox0522e2394ad8813381ce
 
         try:
             resp_get_all_trade = req_get_all_trade.getResponse(access_token)
-            for i in resp_get_all_trade['trades_sold_get_response']['trades']['trade']:
-                print("====>>SELECT TRADE STATUS:%s,TID:%s"%(i["status"],i["tid"]))
+            # for i in resp_get_all_trade['trades_sold_get_response']['trades']['trade']:
+            for i in resp_get_all_trade['trades_sold_increment_get_response']['trades']['trade']:
+                print("====>>SELECT TRADE TID:%s,STATUS:%s"%(i['tid'],i['status']))
                 if i["status"] == "WAIT_SELLER_SEND_GOODS":
-                    print("========>>NEW TRADE NO SEND GOODS TID:%s"%i["tid"])
+                    print("====>>NEW TRADE NO SEND GOODS TID:%s"%i["tid"])
                     req_send_goods.tid = i["tid"]
                     # import pdb;pdb.set_trace();
                     try:
-                        print("========>>BEFORE SEND GOODS TID:%s,STATUS:%s"%(i["tid"],i["status"]))
+                        print("====>>BEFORE SEND GOODS TID:%s,STATUS:%s"%(i["tid"],i["status"]))
                         resp_send_goods = req_send_goods.getResponse(access_token)
                         # ====set tid for get trade full info==== #
                         req_get_one_trade.tid = i["tid"]
                         resp_get_one_trade = req_get_one_trade.getResponse(access_token)
-                        print("========>>AFTER SEND GOODS TID STATUS:%s"%resp_get_one_trade['trade_fullinfo_get_response']['trade']['status'])
+                        print("===>>AFTER SEND GOODS TID STATUS:%s"%resp_get_one_trade['trade_fullinfo_get_response']['trade']['status'])
                         time.sleep(5)
                     except Exception as e:
                         print("====>>AN EXCEPTION:",e)
@@ -44,3 +53,6 @@ def send_goods(client_id="1023079608",client_secret="sandbox0522e2394ad8813381ce
             print("====>>AN EXCEPTION:",e)
             break
         time.sleep(10)
+
+if __name__ == '__main__':
+    send_goods()
